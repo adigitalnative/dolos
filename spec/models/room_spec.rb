cb29199d8e987b6838eabce413d62267056abdcb
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-01 17:21:21 -0400

    DB Buildout: Rooms

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe "A Room" do
+  before(:all) do
+    @room = Room.create(name: "Room One", description: "Room One Description foo")
+  end
+  it "has a name" do
+    expect(@room.name).to eq("Room One")
+  end
+
+  it "has a description" do
+    expect(@room.description).to eq("Room One Description foo")
+  end
+
+  it "has exits" do
+    expect(@room).to respond_to(:exits)
+  end
+end
