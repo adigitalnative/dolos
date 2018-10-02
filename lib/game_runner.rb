602d9da698d1eed5b9acdfff6a5e036ff6ebb2a0
Jacqueline Chenault <adigitalnative@gmail.com>
2018-10-02 15:40:54 -0400

    Loading a game succeeds

--- lib/game_runner.rb
+++ lib/game_runner.rb
@@ -25,14 +25,34 @@ class Dolos::GameRunner
       create_and_begin_new_game
     when "load"
       if saved_games?
-        puts "Loading game #{game.name}"
-        @game.load
+        prompt_for_saved_game
       else
         puts "There is no game to load... type 'help' for help"
         process_input
       end
     else
       process_input
+    end
+  end
+
+  def prompt_for_saved_game
+    available_games = []
+    puts "Please select a previous game:"
+    Game.all.each do |game|
+      available_games << game.id
+      puts "#{game.id}: #{game.name}"
+    end
+
+    input = gets.chomp.to_i
+
+    if available_games.include?(input)
+      @game = Game.find(input)
+      @game.load
+    elsif input == "exit"
+      exit
+    else
+      puts "That's not a valid selection. Please try again"
+      prompt_for_saved_game
     end
   end
 
